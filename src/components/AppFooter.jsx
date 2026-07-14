6c7501edddf7c4d5cbca8beaed5758e3971794b9
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-07-14 23:03:57 +0200

    Add AppHeader and AppFooter components for site navigation and footer structure

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,31 @@
+export default function AppFooter() {
+
+    return (
+
+
+        <footer className="bg-light py-5">
+            <div className="container">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
+                    <div className="col">
+                        <h3>Movies Reviews</h3>
+                        <p>Click here to leave a review of the film.</p>
+                    </div>
+                    <div className="col">
+                        <h3>Quick Links</h3>
+                        <ul className="list-unstyled">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h3>Contact Us</h3>
+                        <p>Email: Movieland@movie.com</p>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+    )
+} 
